8453f8089ed90de54882b6cb9dee911ffaad5fbd
REPAY\aitani <aitani@repay.com>
2023-04-06 16:54:00 +0100

    Register Fluent Validation

--- TwoPointsDistance.API/Program.cs
+++ TwoPointsDistance.API/Program.cs
@@ -1,20 +1,21 @@
 using FluentValidation;
+using FluentValidation.AspNetCore;
 using TwoPointsDistance.Application.Interfaces;
-using TwoPointsDistance.Application.Models;
 using TwoPointsDistance.Application.Services;
 using TwoPointsDistance.Application.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssembly(typeof(DistanceCalculationRequestValidator).Assembly);
 builder.Services.AddTransient<IDistanceCalculationService, DistanceCalculationService>();
-builder.Services.AddScoped<IValidator<DistanceCalculationRequest>, DistanceCalculationRequestValidator>();
 
 var app = builder.Build();
 
--- TwoPointsDistance.Application/Services/DistanceCalculationService.cs
+++ TwoPointsDistance.Application/Services/DistanceCalculationService.cs
@@ -1,4 +1,5 @@
-﻿using TwoPointsDistance.Application.Interfaces;
+﻿using TwoPointsDistance.Application.Extensions;
+using TwoPointsDistance.Application.Interfaces;
 using TwoPointsDistance.Domain.Models;
 
 namespace TwoPointsDistance.Application.Services;
@@ -9,11 +10,11 @@ public class DistanceCalculationService : IDistanceCalculationService
 
     public double Calculate(Point pointA, Point pointB)
     {
-        var cosA = Math.Cos(Radians(pointA.Latitude));
-        var cosB = Math.Cos(Radians(pointB.Latitude));
-        var sinA = Math.Sin(Radians(pointA.Latitude));
-        var sinB = Math.Sin(Radians(pointB.Latitude));
-        var phi = Radians(pointA.Longitude) - Radians(pointB.Longitude);
+        var cosA = Math.Cos(pointA.Latitude.ToRadians());
+        var cosB = Math.Cos(pointB.Latitude.ToRadians());
+        var sinA = Math.Sin(pointA.Latitude.ToRadians());
+        var sinB = Math.Sin(pointB.Latitude.ToRadians());
+        var phi = pointA.Longitude.ToRadians() - pointB.Longitude.ToRadians();
         var cosPhi = Math.Cos(phi);
 
         var cosP = sinA * sinB + cosA * cosB * cosPhi;
@@ -21,10 +22,5 @@ public class DistanceCalculationService : IDistanceCalculationService
         var distance = EarthRadius * Math.Acos(cosP);
 
         return Math.Round(distance);
-    }
-
-    private static double Radians(double angle)
-    {
-        return angle * Math.PI / 180;
     }
 }
